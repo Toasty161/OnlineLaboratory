30acbb512a17c7b20109785d361dc1938f21bdeb
Toasty <mrtoasty161@gmail.com>
2024-03-09 17:56:53 +0200

    Add project files.

---
+++ OnlineLaboratory/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace OnlineLaboratory.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
